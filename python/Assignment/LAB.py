1c8fc38120154877aae69271bfaebfa6dfab0246
ijay2010 <40449991+ijay2010@users.noreply.github.com>
2022-07-12 13:25:34 +0100

    Create LAB.py

---
+++ python/Assignment/LAB.py
@@ -0,0 +1,10 @@
+# hands on how to make an arrow twice as large
+
+print("      * "  *2)
+print("    *   * "  *2)
+print("   *     * "  *2)
+print("  *       * "  *2)
+print(" ***      *** "   *2)
+print("  *        * "   *2)
+print("  *        * "   *2)
+print("  ********** " *2)
